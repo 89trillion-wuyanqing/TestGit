61fb54dc10bd4bdaf00b4466320e8f15e2750f23
wuyanqing <yanqing@89trillion.com>
2021-08-11 15:41:37 +0800

    bugfix4  合并

---
+++ app/test.go
@@ -0,0 +1,35 @@
+/**
+ * @Author wuyanqing
+ * @Date 2021/8/11
+ * @Note
+ **/
+
+package main
+
+/**
+ * @Author wuyanqing
+ * @Description //TODO
+ * @Date 3:15 下午 2021/8/11
+ * @Param
+ * @return
+ **/
+func main() {
+
+}
+
+type User struct {
+	name string
+}
+
+//
+//  CreateUser
+//  @Description:
+//  @param userId
+//  @param age
+//  @param user
+//  @return User
+//  @return error
+//
+func CreateUser(userId string, age int, user *User) (User, error) {
+
+}
